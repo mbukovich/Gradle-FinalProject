91c4524033d44c8b5ec74b6c66b5abac1d798db6
mark.bukovich46@gmail.com <mark.bukovich46@gmail.com>
2021-05-17 21:55:18 -0500

    Implemented Paid and Free Flavors
    Paid and Free Flavors are correctly implemented so that the free
    version of the app displays ads, and the paid version does not display
    ads.

--- JokeLibrary/src/main/java/com/example/jokelibrary/GetJoke.java
+++ JokeLibrary/src/main/java/com/example/jokelibrary/GetJoke.java
@@ -8,6 +8,9 @@ public class GetJoke {
 
     List<String> list = new ArrayList<String>(){{
         add("What do you get when you cross a Jaguar and an Elephant? A sports car with a big trunk.");
+        add("Why do all Java programmers wear glasses? Because they cannot C#.");
+        add("Debugging definition: Being the detective in a crime movie where you are also the murderer.");
+        add("Programmer definition: A machine that turns coffee into code.");
         add("Udacity's Android Developer Nanodegree is very up-to-date...");
     }};
 
--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -27,10 +27,15 @@ public class MainActivityFragment extends Fragment {
         // Create an ad request. Check logcat output for the hashed device ID to
         // get test ads on a physical device. e.g.
         // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
-        mAdView.loadAd(adRequest);
+
+        // Only make an adRequest if we are in the Free Flavor
+        if (BuildConfig.FLAVOR.equals("Free")) {
+            AdRequest adRequest = new AdRequest.Builder()
+                    .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                    .build();
+            mAdView.loadAd(adRequest);
+        }
+
         return root;
     }
 }
